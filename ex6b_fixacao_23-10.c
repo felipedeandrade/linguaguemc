f1bd3fff384769cd8cf9299f1993ef838fc4b5b3
felipedeandrade <31084001+felipedeandrade@users.noreply.github.com>
2017-10-23 21:40:30 -0200

    Add files via upload

---
+++ ex6b_fixacao_23-10.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 6b.Informa a quantidade de notas para um saque.
+
+int main()
+{
+    
+  int notas[7] = {100,50,20,10,5,1};
+  int saque, saque2, qtd_notas,cont;
+  
+  printf ("Digite o valor desejado do saque: ");
+  scanf ("%i", &saque);
+  saque2 = saque;
+  printf ("\n ------ \n\n");
+  
+  for (cont=0; cont<7; cont++)
+  {
+   qtd_notas = saque/notas[cont];
+   if (qtd_notas > 0)
+   {
+                  
+   printf("Sera necessario %i notas de R$%i \n\n", qtd_notas, notas[cont]);
+   if ( saque % notas[cont] != 0)
+   {
+        saque = saque - (qtd_notas * notas[cont]);
+   }
+   else
+   {
+       break;
+   }
+   }
+   
+  }
+  printf ("\n ------ \n\n");
+  printf("Totalizando o valor solicitado de %i.\n\n",saque2); 
+  system("PAUSE");	
+  return 0;
+}
