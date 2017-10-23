e3160f79876642020a5753f0d8d3f51470b5c49c
felipedeandrade <31084001+felipedeandrade@users.noreply.github.com>
2017-10-23 21:54:52 -0200

    Update ex6b_fixacao_23-10.c

--- ex6b_fixacao_23-10.c
+++ ex6b_fixacao_23-10.c
@@ -7,19 +7,20 @@ int main()
 {
     
   int notas[7] = {100,50,20,10,5,1};
+  int qtd[7] = {};
   int saque, saque2, qtd_notas,cont;
   
   printf ("Digite o valor desejado do saque: ");
   scanf ("%i", &saque);
   saque2 = saque;
-  printf ("\n ------ \n\n");
+  printf ("\n------\n\n");
   
   for (cont=0; cont<7; cont++)
   {
    qtd_notas = saque/notas[cont];
    if (qtd_notas > 0)
    {
-                  
+   qtd[cont] = qtd_notas;               
    printf("Sera necessario %i notas de R$%i \n\n", qtd_notas, notas[cont]);
    if ( saque % notas[cont] != 0)
    {
@@ -32,8 +33,8 @@ int main()
    }
    
   }
-  printf ("\n ------ \n\n");
-  printf("Totalizando o valor solicitado de %i.\n\n",saque2); 
+  printf ("\n------ \n\n");
+  printf("Totalizando o valor solicitado de R$%i.\n\n",saque2); 
   system("PAUSE");	
   return 0;
 }
